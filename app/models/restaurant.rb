6554db7c8efba2653340352bbc36258506dfed41
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-20 23:40:37 -0300

    generated restaurants contorller

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   @restaurant_type = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: @restaurant_type}
+  validates :category, inclusion: { in: @restaurant_type }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:new, :create, :index, :show] do
+    resources :reviews, only: [ :new, :create ]
+  end
 end
+
+# A visitor can add a new review to a restaurant
+# GET "restaurants/38/reviews/new"
+# POST "restaurants/38/reviews"
