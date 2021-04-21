068d26fec00252c730f50deff23d0daa1860e7c4
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-20 22:48:09 -0300

    Added validations to models

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_235920) do
+ActiveRecord::Schema.define(version: 2021_04_21_010547) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
     t.text "content"
+    t.integer "restaurant_id", null: false
+    t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
