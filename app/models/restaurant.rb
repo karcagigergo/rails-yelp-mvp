347a8cf6ce68a8d5f29c1428fc74621fe74d028f
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-20 23:14:38 -0300

    made rails db:seed for database

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
+  @restaurant_type = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"]}
+  validates :category, inclusion: { in: @restaurant_type}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurant_type = ["chinese", "italian", "japanese", "french", "belgian"]
+restaurants = [
+  { name: 'Escovinha\'s',
+    address: 'St Ambrose Croft',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Jorge\'s No T-shirt Bakery',
+    address: 'Pope\'s Avenue',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Rachel\'s Deutsches Restaurant',
+    address: 'Shadwell Park Grove',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'CO\'s Brunch',
+    address: 'Pool Darkin Lane',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Capy\'s Lunch',
+    address: 'Staddon Gardens',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Fé Lunch',
+    address: 'Saint Brides Close',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Slack\'s Hero Coffee Shop',
+    address: 'Swanwick Gardens',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Baby Room Coffee Shop',
+    address: 'Fairhurst Way',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample },
+
+  { name: 'Upside Down Desserts',
+    address: 'Jamaica Circle',
+  phone_number: Faker::PhoneNumber.cell_phone.to_s, category: restaurant_type.sample }
+]
+
+restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
