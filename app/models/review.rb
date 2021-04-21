c8ff65b76920ed47b5f28614abc6738990ac8b2d
Gergo Karcagi <karcagigergo@gmail.com>
2021-04-21 02:40:13 -0300

    Added some MORE style

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
   validates :rating, numericality: { only_integer: true }
-  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, inclusion: { in: (0..5),
+  message: "value has to be between 1 and 5" }
 end
